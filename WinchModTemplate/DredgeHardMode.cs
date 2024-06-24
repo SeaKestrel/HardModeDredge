b59bb3fed8b85f8b81f0ac7567ce99a70b619abe
SeaKestrel <jepantal@outlook.fr>
2024-06-24 12:50:02 +0200

    Projet setup

---
+++ WinchModTemplate/DredgeHardMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace DredgeHardMode
+{
+	public class DredgeHardMode : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(DredgeHardMode)} has loaded!");
+		}
+	}
+}
--- WinchModTemplate/Loader.cs
+++ WinchModTemplate/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace DredgeHardMode
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(DredgeHardMode));
+			gameObject.AddComponent<DredgeHardMode>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
